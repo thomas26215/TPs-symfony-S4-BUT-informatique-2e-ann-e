fe93948b42d5822d9d67cc5b0139bc6f98eb709c
Thomas <venouilthomas123456@gmail.com>
2025-02-24 10:59:22 +0100

    Migrations réussies + modifs controlleur produit

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -5,27 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\BoutiqueService;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CategorieRepository;
+use App\Repository\ProduitRepository;
+use App\Entity\Categorie;
 
 class BoutiqueController extends AbstractController
 {
     #[Route('/boutique/{_locale}', name: 'app_boutique', requirements: ['_locale' => '%app.supported_locales%'], defaults: ['_locale' => 'fr'])]
-    public function index(BoutiqueService $boutique): Response
+    public function index(CategorieRepository $categorieRepository): Response
     {
-        $categories = $boutique->findAllCategories();
+        $categories = $categorieRepository->findAll();
         return $this->render('boutique/index.html.twig', [
             'categories' => $categories,
         ]);
     }
 
     #[Route('/rayon/{idCategorie}/{_locale}', name: 'app_boutique_rayon', requirements: ['_locale' => '%app.supported_locales%'], defaults: ['_locale' => 'fr'])]
-    public function rayon(int $idCategorie, BoutiqueService $boutique): Response
+    public function rayon(int $idCategorie, CategorieRepository $categorieRepository, ProduitRepository $produitRepository): Response
     {
-        $categorie = $boutique->findCategorieById($idCategorie);
+        $categorie = $categorieRepository->find($idCategorie);
         if (!$categorie) {
             throw $this->createNotFoundException('La catégorie demandée n\'existe pas');
         }
-        $produits = $boutique->findProduitsByCategorie($idCategorie);
+        $produits = $produitRepository->findBy(['categorie' => $categorie]);
 
         return $this->render('boutique/rayon.html.twig', [
             'categorie' => $categorie,
@@ -33,9 +36,11 @@ class BoutiqueController extends AbstractController
         ]);
     }
 
-    public function topVentes(BoutiqueService $boutique): Response
+    public function topVentes(ProduitRepository $produitRepository): Response
     {
-        $produits = $boutique->getTopVentes();
+        // Supposons que vous ayez une méthode pour obtenir les meilleures ventes
+        // Si ce n'est pas le cas, vous devrez l'implémenter dans ProduitRepository
+        $produits = $produitRepository->findTopVentes();
         return $this->render('boutique/topVentes.html.twig', [
             'produits' => $produits,
         ]);
